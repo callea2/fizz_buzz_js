2819a066174923d4481820cd42e490ae9707fed0
Carl-Philip <callea2@gmail.com>
2016-05-08 21:04:49 +0200

    WIP: Attempted to make a working fizz_buzz.js app. Updated index.html to reflect changes

--- assets/js/fizz_buzz.js
+++ assets/js/fizz_buzz.js
@@ -1,14 +1,22 @@
-function Fizz_Buzz() {
-};
-
-for (var i=1; i <= 100; i++)
-{
-    if (i % 15 == 0)
-        console.log("FizzBuzz");
-    else if (i % 5 == 0)
-        console.log("Buzz");
-    else if (i % 3 == 0)
-        console.log("Fizz");
-    else
-        console.log(i);
-}
+$(document).ready(function () {
+  $('#calculate').click(function () {
+    var number = parseFloat($('#input').val());
+    console.log(number)
+    var FizzBuzz = function (number)
+      for (var i=1; i<=number; i++)
+      {
+          if (i % 15 == 0)
+              console.log("FizzBuzz");
+              window.alert("FizzBuzz");
+          else if (i % 5 == 0)
+              console.log("Buzz");
+              window.alert("Buzz");
+          else if (i % 3 == 0)
+              console.log("Fizz");
+              window.alert("Fizz");
+          else
+              console.log(i);
+              window.alert(" + i +");
+      }
+  });
+});

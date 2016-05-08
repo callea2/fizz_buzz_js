b7693890cb56bb8d26c988bafd6e13195c55c607
Carl-Philip <callea2@gmail.com>
2016-05-08 19:24:24 +0200

    Added index with basic html structure

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -8,4 +8,12 @@ describe("Fizz_Buzz"), function() {
   it("returns 'fizz' if number is divisible by 3", function() {
     expect(fizz_buzz(3)).toEqual('Fizz');
   });
+
+  it("returns 'buzz' if number is divisible by 5", function() {
+    expect(fizz_buzz(5)).toEqual('Buzz');
+  });
+
+  it("returns 'fizzbuzz' if number is divisible by 15", function() {
+    expect(fizz_buzz(15)).toEqual('FizzBuzz');
+  });
 };

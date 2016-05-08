3d9a558d88c3e14b8a00996850067ca3748168a4
Carl-Philip <callea2@gmail.com>
2016-05-08 18:51:43 +0200

    Due to unexpected and seemingly unrelated error with karma/jasmine this commit includes fully updated src and spec for fizzbuzz

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -1,1 +1,11 @@
+describe("Fizz_Buzz"), function() {
+  var fizz_buzz;
 
+  beforeEach(function() {
+    fizz_buzz = new Fizz_Buzz();
+  });
+
+  it("returns 'fizz' if number is divisible by 3", function() {
+    expect(fizz_buzz(3)).toEqual('Fizz');
+  });
+};
